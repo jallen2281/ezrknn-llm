2bc52e1be8b6012c8f85ec80f5ea39bc0960705d
Pelochus <anghflores@gmail.com>
2025-05-28 11:48:05 +0200

    Change conversion from CUDA to CPU

--- examples/DeepSeek-R1-Distill-Qwen-1.5B_Demo/export/export_rkllm.py
+++ examples/DeepSeek-R1-Distill-Qwen-1.5B_Demo/export/export_rkllm.py
@@ -8,16 +8,16 @@ https://huggingface.co/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
 Download the DeepSeek R1 model from the above url.
 '''
 
-modelpath = '/path/to/DeepSeek-R1-Distill-Qwen-1.5B'
+modelpath = 'DeepSeek-R1-Distill-Qwen-1.5B'
 llm = RKLLM()
 
 # Load model
 # Use 'export CUDA_VISIBLE_DEVICES=0' to specify GPU device
 # device options ['cpu', 'cuda']
 # dtype  options ['float32', 'float16', 'bfloat16']
-# Using 'bfloat16' or 'float16' can significantly reduce memory consumption but at the cost of lower precision  
-# compared to 'float32'. Choose the appropriate dtype based on your hardware and model requirements. 
-ret = llm.load_huggingface(model=modelpath, model_lora = None, device='cuda', dtype="float32", custom_config=None, load_weight=True)
+# Using 'bfloat16' or 'float16' can significantly reduce memory consumption but at the cost of lower precision
+# compared to 'float32'. Choose the appropriate dtype based on your hardware and model requirements.
+ret = llm.load_huggingface(model=modelpath, model_lora = None, device='cpu', dtype="float32", custom_config=None, load_weight=True)
 # ret = llm.load_gguf(model = modelpath)
 if ret != 0:
     print('Load model failed!')
@@ -27,8 +27,8 @@ if ret != 0:
 dataset = "./data_quant.json"
 # Json file format, please note to add prompt in the input，like this:
 # [{"input":"Human: 你好！\nAssistant: ", "target": "你好！我是人工智能助手KK！"},...]
-# Different quantization methods are optimized for different algorithms:  
-# w8a8/w8a8_gx   is recommended to use the normal algorithm.  
+# Different quantization methods are optimized for different algorithms:
+# w8a8/w8a8_gx   is recommended to use the normal algorithm.
 # w4a16/w4a16_gx is recommended to use the grq algorithm.
 qparams = None # Use extra_qparams
 target_platform = "RK3588"
